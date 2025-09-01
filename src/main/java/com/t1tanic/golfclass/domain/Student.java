fa4f5ba9eb7ce889dd2416c2168dbca6aafec5ea
Gabriel Vendramini <gabriel_vendramini@hotmail.com>
2025-09-01 22:55:52 +0200

    Model golf class entities

---
+++ src/main/java/com/t1tanic/golfclass/domain/Student.java
@@ -0,0 +1,34 @@
+package com.t1tanic.golfclass.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @OneToMany(mappedBy = "student", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Enrollment> enrollments = new HashSet<>();
+}
+
